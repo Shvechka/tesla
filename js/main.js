74a7710cc425b38d961cee88e70d0218078e2624
Shvechka <66243838+Shvechka@users.noreply.github.com>
2020-07-02 18:14:59 +0400

    tesla main version

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+
+$(function () {
+  $(".header__slaider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    fade: true,
+    dots: true,
+    appendDots:$('.header__top-inner')
+  });
+});
